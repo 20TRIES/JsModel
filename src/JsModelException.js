21f51b0f1c37a1af0a300c31870bef0509643a6b
Marcus T <marcust261@icloud.com>
2016-06-28 16:03:01 +0100

    Added model exception.

---
+++ src/JsModelException.js
@@ -0,0 +1,26 @@
+/**
+ * A base exception class that all exceptions within the js_model package will extend.
+ */
+export default class JsModelException{
+    /**
+     * Constructor
+     *
+     * @param {String} message
+     */
+    constructor(message) {
+
+        /**
+         * @type {String}
+         */
+        this.message = message;
+    }
+
+    /**
+     * Gets a string representation of an object.
+     *
+     * @return {String}
+     */
+    toString() {
+        return this.message;
+    }
+}
